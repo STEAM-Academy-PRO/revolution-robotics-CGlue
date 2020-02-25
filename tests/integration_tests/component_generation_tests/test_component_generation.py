0b18476d1ae59d7ab96a6a2af15869c9f14acf4b
Dániel Buga <daniel@revolutionrobotics.org>
2020-02-25 19:42:10 +0100

    Move tests to their right place

--- tests/integration_tests/component_generation_tests/test_component_generation.py
+++ tests/integration_tests/component_generation_tests/test_component_generation.py
@@ -8,10 +8,10 @@ from cglue.plugins.ProjectConfigCompactor import project_config_compactor
 
 
 class TestComponentGeneration(unittest.TestCase):
-    def test_complex_connection_does_not_cause_error_when_consumer_is_in_list(self):
+    def test_component_generation_does_not_raise_error(self):
         os.chdir(os.path.dirname(__file__))
 
-        generator = cglue.CGlue("fixtures/00-demo-test/project_consumer_list.json")
+        generator = cglue.CGlue("../fixtures/00-demo-test/project_consumer_list.json")
 
         generator.add_plugin(project_config_compactor())
         generator.add_plugin(builtin_data_types())
@@ -19,17 +19,4 @@ class TestComponentGeneration(unittest.TestCase):
 
         generator.load()
 
-        generator.generate_runtime('runtime_file.c')
-
-    def test_complex_connection_does_not_cause_error_when_consumer_is_by_itself(self):
-        os.chdir(os.path.dirname(__file__))
-
-        generator = cglue.CGlue("fixtures/00-demo-test/project_single_consumer.json")
-
-        generator.add_plugin(project_config_compactor())
-        generator.add_plugin(builtin_data_types())
-        generator.add_plugin(runtime_events())
-
-        generator.load()
-
-        generator.generate_runtime('runtime_file.c')
+        generator.update_component('foo')
--- tests/integration_tests/runtime_generation/test_runtime_generation.py
+++ tests/integration_tests/runtime_generation/test_runtime_generation.py
@@ -1,5 +1,36 @@
 import unittest
+import os
+
+from cglue import cglue
+from cglue.plugins.BuiltinDataTypes import builtin_data_types
+from cglue.plugins.RuntimeEvents import runtime_events
+from cglue.plugins.ProjectConfigCompactor import project_config_compactor
 
 
 class TestRuntimeGeneration(unittest.TestCase):
-    pass
+    def test_complex_connection_does_not_cause_error_when_consumer_is_in_list(self):
+        os.chdir(os.path.dirname(__file__))
+
+        generator = cglue.CGlue("../fixtures/00-demo-test/project_consumer_list.json")
+
+        generator.add_plugin(project_config_compactor())
+        generator.add_plugin(builtin_data_types())
+        generator.add_plugin(runtime_events())
+
+        generator.load()
+
+        generator.generate_runtime('runtime_file.c')
+
+    def test_complex_connection_does_not_cause_error_when_consumer_is_by_itself(self):
+        os.chdir(os.path.dirname(__file__))
+
+        generator = cglue.CGlue("../fixtures/00-demo-test/project_single_consumer.json")
+
+        generator.add_plugin(project_config_compactor())
+        generator.add_plugin(builtin_data_types())
+        generator.add_plugin(runtime_events())
+
+        generator.load()
+
+        generator.generate_runtime('runtime_file.c')
+

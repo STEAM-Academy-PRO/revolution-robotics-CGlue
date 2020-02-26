e6351a299429f41556d981465343ff884e490d28
Dániel Buga <daniel@revolutionrobotics.org>
2020-02-26 11:24:31 +0100

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CGlue',
-    version='0.1.1',
+    version='0.1.2',
     packages=['cglue', 'cglue/plugins', 'cglue/utils'],
     package_dir={'cglue': 'cglue'},
     url='https://github.com/RevolutionRobotics/CGlue',

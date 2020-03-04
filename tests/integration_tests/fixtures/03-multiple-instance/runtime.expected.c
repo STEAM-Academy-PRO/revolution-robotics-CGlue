c5cba4e3123ff05beec298ffa7efae104d83bd94
Dániel Buga <daniel@revolutionrobotics.org>
2020-03-04 21:33:51 +0100

    Test and fix multiple instance port connections

--- tests/integration_tests/fixtures/03-multiple-instance/runtime.expected.c
+++ tests/integration_tests/fixtures/03-multiple-instance/runtime.expected.c
@@ -4,6 +4,7 @@
 static foo_Instance_t foo_instance_foo1;
 static foo_Instance_t foo_instance_foo2;
 static uint32_t foo1_WPort_variable = 0u;
+static uint32_t foo2_WPort_variable = 0u;
 
 void Runtime_RaiseEvent_OnInit(void)
 {
@@ -17,6 +18,10 @@ void foo_Write_WPort(foo_Instance_t* instance, uint32_t value)
     {
         foo1_WPort_variable = value;
     }
+    if (instance == &foo_instance_foo2)
+    {
+        foo2_WPort_variable = value;
+    }
 }
 
 uint32_t foo_Read_RPort(foo_Instance_t* instance)
@@ -25,6 +30,10 @@ uint32_t foo_Read_RPort(foo_Instance_t* instance)
     if (instance == &foo_instance_foo2)
     {
         return_value = foo1_WPort_variable;
+    }
+    if (instance == &foo_instance_foo1)
+    {
+        return_value = foo2_WPort_variable;
     }
     return return_value;
 }

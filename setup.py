4a29c6fb7f0d3372f501c3f9e5d80b2fec322bb1
Dániel Buga <daniel@revolutionrobotics.org>
2020-03-16 13:47:12 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ py_version = sys.version_info
 
 setup(
     name='CGlue',
-    version='0.1.15',
+    version='0.2.0',
     packages=['cglue', 'cglue/plugins', 'cglue/utils'],
     package_dir={'cglue': 'cglue'},
     url='https://github.com/RevolutionRobotics/CGlue',
